a713956901b318dbab4679fb47ca4b5b46d2ac11
Prerana Blown Lama <prerana.lama@startsml.com>
2026-01-12 15:50:18 +0545

    updates: .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,12 @@
 export const registerUser = async (data) => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/register`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    }
-  );
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
 
   if (!response.ok) {
     throw new Error("Request failed");
   }
-
   return response.json();
 };
